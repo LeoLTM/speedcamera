1108124072b0e9894b559d8d6a1adfc020a41e1d
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-12-01 18:03:00 +0100

    changed default window size

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -9,8 +9,8 @@ const inDevelopment = process.env.NODE_ENV === "development";
 function createWindow() {
     const preload = path.join(__dirname, "preload.js");
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1280,
+        height: 720,
         webPreferences: {
             devTools: true,
             contextIsolation: true,

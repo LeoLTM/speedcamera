e6ede733d6104158c573c864eb2a6ad9f8aa2fae
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-12-02 00:30:32 +0100

    added serial slice for managing available and selected serial ports

--- electron/src/stores/useStore.ts
+++ electron/src/stores/useStore.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 import { CameraSlice, createCameraSlice } from './cameraSlice';
+import { SerialSlice, createSerialSlice } from './serialSlice';
 
-export const useStore = create<CameraSlice>()((...a) => ({
+export const useStore = create<CameraSlice & SerialSlice>()((...a) => ({
     ...createCameraSlice(...a),
+    ...createSerialSlice(...a)
 }));

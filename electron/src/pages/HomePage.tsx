88deb826d03409c5b22a40bb11029608fb8d0565
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-03-24 01:10:07 +0100

    move onStatus() listener to homepage

--- electron/src/pages/HomePage.tsx
+++ electron/src/pages/HomePage.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ToggleTheme from "@/components/ToggleTheme";
-import LangToggle from "@/components/LangToggle";
 import CameraComponent from "@/components/CameraComponent";
-import CameraSelector from "@/components/CameraSelector";
-import BoardSelector from "@/components/BoardSelector";
-import MeasurementData from "@/components/MeasurementData";
+import Measurements from "@/components/Measurements";
 import Devices from "@/components/Devices";
+import { useStore } from "@/stores/useStore";
+import { toast } from "sonner";
+import { Status, StatusMessage } from "@/types/status";
 
 
 export default function HomePage() {
+
+    const { selectedCameraRef, lastMeasurement } = useStore();
+    const { setLastMeasurement } = useStore();
+
+    useEffect(() => {
+        const handleStatus = (jsonStatus: string) => {
+            const status: StatusMessage = JSON.parse(jsonStatus);
+            switch(status.status) {
+                case Status.JSON_ERROR:
+                    toast.error(`A JSON error occurred!`);
+                    break;
+                case Status.FLASH:
+                    toast.success(`Flash successful!`);
+                    break;
+                case Status.SPEEDING:
+                    setLastMeasurement(status.value!);
+                    break;
+                case Status.LEGAL:
+                    setLastMeasurement(status.value!);
+                    break;
+                case Status.CONFIG:
+                    toast.info(`Updated max speed to ${status.value} km/h`);
+                    break;
+            }
+        }
+        window.serial.onStatus(handleStatus);
+
+        return () => { 
+            window.serial.offStatus(handleStatus);
+        }
+    }, []);
+
     return (
         <>
             <div className="flex h-screen flex-col items-center justify-center gap-2">
@@ -18,7 +50,7 @@ export default function HomePage() {
                 <ToggleTheme />
                 <div className="flex flex-row gap-2">
                     <Devices />
-                    <MeasurementData />
+                    <Measurements />
                 </div>
 
                 <CameraComponent />

b71d5306b8b8e1c57b4977036241015ebdcee74b
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-03-24 01:09:47 +0100

    move flash triggering when maxSpeed is exceeded to slice logic

--- electron/src/stores/measurementSlice.ts
+++ electron/src/stores/measurementSlice.ts
@@ -1,4 +1,5 @@
 import { playBeepSound } from '@/helpers/sound/audio';
+import { SerialCommand, SerialCommands } from '@/types/commands';
 import { StateCreator } from 'zustand';
 
 export interface MeasurementSlice {
@@ -18,12 +19,21 @@ export const createMeasurementSlice: StateCreator<
     maxSpeed: 2,
     setLastMeasurement: (measurement: number) => {
         const { maxSpeed } = get();
-        if (measurement > maxSpeed) {
+        if (measurement > maxSpeed) {   
             playBeepSound();
+            const serialCommand: SerialCommand = {
+                command: SerialCommands.FLASH,
+            };
+            window.serial.sendCommand(JSON.stringify(serialCommand));
         }
         set({ lastMeasurement: measurement });
     },
     setMaxSpeed: (speed: number) => {
+        const serialCommand: SerialCommand = {
+            command: SerialCommands.SET_MAX_SPEED,
+            value: speed,
+        };
+        window.serial.sendCommand(JSON.stringify(serialCommand));
         set({ maxSpeed: speed });
     }
 })

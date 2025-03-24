d20a0d779d5196788dd5a52c3b740f7b1c03fcd5
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-03-24 01:10:39 +0100

    remove onStatus listener and related logic from Measurements component

--- electron/src/components/Measurements.tsx
+++ electron/src/components/Measurements.tsx
@@ -20,37 +20,12 @@ export default function Measurements() {
     const { lastMeasurement, maxSpeed } = useStore();
     const { setLastMeasurement, setMaxSpeed } = useStore();
 
-    useEffect(() => {
-        window.serial.onStatus((jsonStatus) => {
-            const status: StatusMessage = JSON.parse(jsonStatus);
-            console.log(`Received status: ${status.status}`);
-            switch(status.status) {
-                case Status.LEGAL:
-                    if(status.value) setLastMeasurement(status.value);
-                    break;
-                case Status.SPEEDING:
-                    if(status.value) setLastMeasurement(status.value);
-                    break;
-                case Status.CONFIG:
-                    if(status.value) toast(`Updated max speed to ${status.value} km/h`);
-                    break;
-                default:
-                    break;
-            }
-        })
-    });
-
     function handleSetMaxSpeed(maxSpeed: number) {
         // Check if maxSpeed is a number and between 1 and 199
         if(isNaN(maxSpeed) || maxSpeed < 1 || maxSpeed > 199) {
             toast("Max speed must be a number between 1 and 199");
             return;
         }
-        const serialCommand: SerialCommand = {
-            command: SerialCommands.SET_MAX_SPEED,
-            value: maxSpeed,
-        };
-        window.serial.sendCommand(JSON.stringify(serialCommand));
         setMaxSpeed(maxSpeed);
     }
 

772483106b5c1fea5e1bf472c33421c3a4b94aab
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-03-24 22:19:12 +0100

    add pictureDelay, flashDelay, and flashDuration state management in cameraSlice

--- electron/src/stores/cameraSlice.ts
+++ electron/src/stores/cameraSlice.ts
@@ -1,14 +1,21 @@
 import { RefObject } from 'react';
 import Webcam from 'react-webcam';
 import { StateCreator } from 'zustand';
+import { SerialCommand, SerialCommands } from '@/types/commands';
 
 export interface CameraSlice {
     availableCameras: MediaDeviceInfo[];
     selectedCamera: MediaDeviceInfo | null;
     selectedCameraRef: RefObject<Webcam> | null;
+    pictureDelay: number;
+    flashDelay: number;
+    flashDuration: number;
     setAvailableCameras: (cameras: MediaDeviceInfo[]) => void;
     setSelectedCamera: (camera: MediaDeviceInfo | undefined) => void;
     setSelectedCameraRef: (camera: RefObject<Webcam> | null) => void;
+    setPictureDelay: (delay: number) => void;
+    setFlashDelay: (delay: number) => void;
+    setFlashDuration: (duration: number) => void;
 }
 
 export const createCameraSlice: StateCreator<
@@ -20,6 +27,9 @@ export const createCameraSlice: StateCreator<
     availableCameras: [],
     selectedCamera: null,
     selectedCameraRef: { current: null },
+    pictureDelay: 100,
+    flashDelay: 100,
+    flashDuration: 50,
     setAvailableCameras: (cameras: MediaDeviceInfo[]) => {
         set({ availableCameras: cameras });
     },
@@ -32,5 +42,24 @@ export const createCameraSlice: StateCreator<
     },
     setSelectedCameraRef: (camera: RefObject<Webcam> | null) => {
         set({ selectedCameraRef: camera });
-    }
+    },
+    setPictureDelay: (delay: number) => {
+        set({ pictureDelay: delay });
+    },
+    setFlashDelay: (delay: number) => {
+        const serialCommand: SerialCommand = {
+            command: SerialCommands.SET_FLASH_DELAY,
+            value: delay,
+        };
+        window.serial.sendCommand(JSON.stringify(serialCommand));
+        set({ flashDelay: delay });
+    },
+    setFlashDuration: (duration: number) => {
+        const serialCommand: SerialCommand = {
+            command: SerialCommands.SET_FLASH_DURATION,
+            value: duration,
+        };
+        window.serial.sendCommand(JSON.stringify(serialCommand));
+        set({ flashDuration: duration });
+    },
 })

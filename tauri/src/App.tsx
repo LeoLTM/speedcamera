a6a4b9b7dcf6c93eec94aa36f47eca59610290b2
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-09-25 23:18:56 +0200

    added Tauri 2.0 boilerplate including shadcn/ui

---
+++ tauri/src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <div className="flex flex-col items-center justify-center">
+        <h1>Welcome to Tauri + React</h1>
+
+        <form
+          className="flex flex-row "
+          onSubmit={(e) => {
+            e.preventDefault();
+            greet();
+          }}
+        >
+          <Input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+          <Button type="submit">Greet</Button>
+        </form>
+        <p>{greetMsg}</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
